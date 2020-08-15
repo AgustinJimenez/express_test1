a252a18f8211eeea9b8a29862c7baf2ec6947576
Agustin <agus.jimenez.caba@gmail.com>
2020-08-15 00:09:41 -0400

    set logger on exception handler

--- src/setup/ExceptionSetup.ts
+++ src/setup/ExceptionSetup.ts
@@ -1,4 +1,4 @@
+import log from "../providers/LoggerProvider"
 export default (error: any) => {
-	console.error("EXCEPTION HANDLER ====> ", error)
-	// response.status(INTERNAL_SERVER_ERROR).send(error.message)
+	log.error(error)
 }
